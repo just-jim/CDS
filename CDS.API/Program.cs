3e3afd3a5fc31e8daff12a4f6d9c532a154eee24
just_jim <dimitris@pleo.io>
2024-05-01 07:08:41 +0200

    Automate the database migrations
    - Run the migration on application entry point

--- CDS.API/Program.cs
+++ CDS.API/Program.cs
@@ -1,6 +1,8 @@
 using CDS.API.Controllers;
 using CDS.Application;
 using CDS.Infrastructure;
+using CDS.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -15,6 +17,14 @@ var app = builder.Build();
     if (app.Environment.IsDevelopment()) {
         app.UseSwagger();
         app.UseSwaggerUI();
+    }
+    
+    // Run the database migrations
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetRequiredService<CdsDbContext>();
+        dbContext.Database.Migrate();
     }
 
     app.MapControllers();
